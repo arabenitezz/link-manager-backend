f13b861f67b274e9ddb2cdb6756ab27af340c35b
arabenitezz <abenitezr18@gmail.com>
2025-01-16 17:58:30 -0300

    update server.js

--- back/server.js
+++ back/server.js
@@ -22,5 +22,5 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 // Puerto
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));

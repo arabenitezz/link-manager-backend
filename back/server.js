73b439ebc6ddcebab18bf220693407e26af705bc
arabenitezz <abenitezr18@gmail.com>
2025-01-18 13:48:22 -0300

    Update server.js

--- back/server.js
+++ back/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     'https://link-manager-vanilla-js.vercel.app',
-    'http://127.0.0.1:5173',
+    'https://link-manager-vue.vercel.app/',
     'http://localhost:5173',
     'http://localhost:3000'
   ],

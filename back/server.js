8b856665e16368e97520d5dbc3af1d9cbb3930d7
arabenitezz <abenitezr18@gmail.com>
2025-01-18 12:25:24 -0300

    Update server.js

--- back/server.js
+++ back/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: [
     'https://link-manager-vanilla-js.vercel.app',
     'http://127.0.0.1:5173',
-    'http://localhost:5500',
+    'http://localhost:5173',
     'http://localhost:3000'
   ],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
